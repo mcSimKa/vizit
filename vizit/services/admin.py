e3e72ca35c589b893f74717685cdff0d50a336fa
Maksym Kysunko <maksym.kysunko@aptem.co.uk>
2023-01-02 23:24:54 +0200

    make migrations +createsuperuser+ enable admin CMS

--- vizit/services/admin.py
+++ vizit/services/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Service, ServiceLocation, Address
+
+admin.site.register(Service)
+admin.site.register(Address)
+admin.site.register(ServiceLocation)
--- vizit/services/models.py
+++ vizit/services/models.py
@@ -6,12 +6,17 @@ class Service(models.Model):
     Phone = models.CharField(max_length=32)
     Address = models.CharField(max_length=100)
     url = models.URLField()
+    def __str__(self) -> str:
+        return self.Name+'('+self.url+')'
 
 class Address(models.Model):
     CityName = models.CharField(max_length=100)
     StreetName = models.CharField(max_length=100)
     Building = models.CharField(max_length=12)
     Room = models.CharField(max_length=5)
+
+    def __str__(self) -> str:
+        return '('+self.CityName+')|('+self.StreetName+')|('+self.Building+')'
 
 class ServiceLocation(models.Model):
     ServiceId = models.ForeignKey(Service, on_delete=models.CASCADE)
