3ca9d806b3027481751b0ee6db89ef41f3e89ad2
Maksym Kysunko <mkisunko@gmail.com>
2023-01-14 21:27:10 +0200

    trying to ghet gitignboire be applied ot the project

--- geodata/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Language,Country, Address
-# Register your models here.
-admin.site.register(Address)
-admin.site.register(Language)
-admin.site.register(Country)
